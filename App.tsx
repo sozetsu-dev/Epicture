a0f7b3e7313247f3a82c81c7794f812bbac253b7
Thomas Pentegno <thomas.pentegno@epitech.eu>
2021-10-14 17:37:26 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './hooks/useCachedResources';
+import Navigation from './navigation';
+import HomeScreen from './screens/HomeScreen';
+import { AuthProvider } from './utils/auth';
+
+export default function App(): JSX.Element | null {
+  const isLoadingComplete = useCachedResources();
+
+  if (!isLoadingComplete) {
+    return null;
+  }
+  return (
+    <SafeAreaProvider>
+      <AuthProvider>
+        <StatusBar style="light" />
+        <Navigation />
+      </AuthProvider>
+    </SafeAreaProvider>
+  );
+}
